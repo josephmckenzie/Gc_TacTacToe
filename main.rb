b04cd6b44177051ac70f3485c149ae7233497b43
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-08-29 11:04:21 -0400

    put board visualtion in a functionthat takes a a message

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-def board_status
+def board_status(message)
 
+puts message
 puts """
    |  |
   1| 2| 3
@@ -11,4 +12,5 @@ puts """
   7| 8| 9
    |  | """
    end
- board_status
+ board_status("Welcome to TicTacToe")
+ board_status("The Current board is:")
